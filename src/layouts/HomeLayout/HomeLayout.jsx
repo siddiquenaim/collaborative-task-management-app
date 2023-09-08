210d98bf40d917410a55be7e836e02869da62384
siddiquenaim <naim.niter.tex06@gmail.com>
2023-09-08 12:33:13 +0600

    Completed tailwind setup

---
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../../components/Home/Home";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default HomeLayout;
