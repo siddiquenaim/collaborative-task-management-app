388ca0a1ca501fad9d015b28ff284048d27a05e5
siddiquenaim <naim.niter.tex06@gmail.com>
2023-09-08 15:14:00 +0600

    added login form

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,8 @@ const Banner = () => {
   return (
     <div className="hero min-h-screen bg-banner">
       <div className="hero-content text-neutral-content">
-        <div className="lg:flex gap-5">
-          <div className="lg:w-[50%]">
+        <div className="lg:flex gap-14 items-center">
+          <div className="lg:w-[50%] mx-auto">
             <h1 className="mb-5 text-5xl font-bold">Track and Manage Task</h1>
             <p className="mb-5 lg:mx-auto text-xl">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
@@ -23,7 +23,48 @@ const Banner = () => {
           ""
         )} */}
           </div>
-          <div className="lg:w-[50%]">div-2</div>
+          <div className="lg:w-[50%] mx-auto">
+            <div className="flex justify-center items-center">
+              <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form className="card-body">
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="email"
+                      className="input input-bordered"
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="password"
+                      className="input input-bordered"
+                    />
+                    <label className="label">
+                      <a href="#" className="label-text-alt link link-hover">
+                        Forgot password?
+                      </a>
+                    </label>
+                  </div>
+                  <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                  </div>
+                  <div className="text-black text-center mt-2">
+                    Already has an account?{" "}
+                    <a href="/register" className="text-blue-500">
+                      Register
+                    </a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
